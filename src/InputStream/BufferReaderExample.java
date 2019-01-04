f505888680bda977e64f34d855b2c8ac9ca015e4
Md Jahidul Islam <jahidul@Mds-MacBook-Pro.fios-router.home>
2019-01-03 20:51:41 -0500

    Updated with  System.getProperty()

--- src/InputStream/BufferReaderExample.java
+++ src/InputStream/BufferReaderExample.java
@@ -2,24 +2,53 @@ package InputStream;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class BufferReaderExample {
     public static void main(String args[])throws Exception{
-        //File filepath = new File(System.getProperty("user.dir") +  "/src/resources/Test.txt");
-      // FileReader fr = new FileReader(filepath.getAbsoluteFile());
 
-        FileReader fileReader =new FileReader("/Users/jahidul/IdeaProjects/JavaBasic1212/src/resources/Test.txt");
+        FileReader fileReader = null;
+        BufferedReader br = null;
+        File filepath = null;
 
-        BufferedReader br = new BufferedReader(fileReader);
+        /**
+         * System.getProperty("user.dir") = Users/jahidul/IdeaProjects/JavaBasic1212
+         *
+         * */
 
-        int i;
-        while((i=br.read())!=-1){
-            System.out.println((char)i);
-        }
+      try {
 
-        br.close();
-        fileReader.close();
+
+         /* fileReader = new FileReader("/Users/jahidul/IdeaProjects/JavaBasic1212/src/resources/Test.txt");
+          br = new BufferedReader(fileReader);*/
+
+          filepath = new File(System.getProperty("user.dir") +  "/src/resources/Test.txt");
+          fileReader = new FileReader(filepath.getAbsoluteFile());
+
+          br = new BufferedReader(fileReader);
+
+
+          int i;
+          while((i=br.read())!=-1){
+              System.out.println((char)i);
+          }
+          }
+      catch (FileNotFoundException e){
+
+          e.printStackTrace();
+          System.out.println("Check file location");
+
+      }
+
+
+
+     finally {
+          br.close();
+          fileReader.close();
+          System.out.println("Leaving finally block");
+      }
+
     }
 }
 
